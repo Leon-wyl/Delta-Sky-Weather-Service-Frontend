80acfe1485674fff20c05a7737cfcb9ed7b8f568
YILANG_WU\yilan <yilangwu@outlook.com>
2023-04-18 07:58:58 +1000

    comment out webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,54 +1,54 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    basicApp: {
-      import: "./src/main.jsx",
-      filename: "basicApp.js",
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, "build/static"),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'src'),
-    },
-    compress: true,
-    port: 4001,
-  },
-  resolve: {
-    extensions: [".jsx", ".js", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-  ],
-};
+// module.exports = {
+//   entry: {
+//     basicApp: {
+//       import: "./src/main.jsx",
+//       filename: "basicApp.js",
+//     },
+//   },
+//   output: {
+//     path: path.resolve(__dirname, "build/static"),
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'src'),
+//     },
+//     compress: true,
+//     port: 4001,
+//   },
+//   resolve: {
+//     extensions: [".jsx", ".js", ".json"],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js)x?$/,
+//         exclude: /node_modules/,
+//         use: [
+//           {
+//             loader: "babel-loader",
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+//         type: "asset/resource",
+//       },
+//       {
+//         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+//         type: "asset/inline",
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./index.html",
+//     }),
+//   ],
+// };
