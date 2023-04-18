92075686e261153b8e57743ac042100f22715624
YILANG_WU\yilan <yilangwu@outlook.com>
2023-04-19 00:27:30 +1000

    created api for wind efficiency

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,48 +2,36 @@ import { postAxios } from "./base.js";
 
 export const fetchWindEfficiency = async (current_windspeed, max_windspeed) => {
   try {
-    // const url =
-    //   "https://5qmp4gs3ud.execute-api.ap-southeast-2.amazonaws.com/F12A_PAPA/wind";
-    // const data = {
-    //   current_windspeed,
-    //   max_windspeed,
-    // };
-    // const headers = {
-    //   "Content-Type": "application/json",
-    //   Accept: "application/json",
-    // };
-    // const res = await postAxios(url, data, headers);
-    // console.log(res);
-    const data = JSON.stringify({
+    const url =
+      "https://5qmp4gs3ud.execute-api.ap-southeast-2.amazonaws.com/F12A_PAPA/wind";
+    const dataWind = {
+      current_windspeed,
+      max_windspeed,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    const res = await postAxios(url, dataWind, headers);
+    console.log(res);
+    console.log(await res.json())
+
+    const dataNews = JSON.stringify({
       query: `{
-  
         contentSearch(q:{
-      
-      operation: AND,
-      
-      subQueries: [
-      
-          {term: "weather"},
-      
-          {term: "sydney"}
-      
-      ]
-      
+          operation: AND,
+          subQueries: [
+            {term: "weather"},
+            {term: "sydney"}
+          ]
         }) {
-      
           total
-      
-      results {
-      
-      webTitle
-      
+          results {
+            webTitle
           }
-      
         }
-      
       }`
     });
-    // const query = "{ getObject(file: 'F14A_DELTA-1681207633') }";
     const response = await fetch(
       "/api/H09A_FOXTROT/graphql",
       {
@@ -53,7 +41,7 @@ export const fetchWindEfficiency = async (current_windspeed, max_windspeed) => {
           "Accept": "application/json",
           "Access-Control-Allow-Origin": "*",
         },
-        body: data,
+        body: dataNews,
       }
     )
     console.log(response)
