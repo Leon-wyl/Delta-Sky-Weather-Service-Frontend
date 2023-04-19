80c8a693668efcc4e5cee5634d2ed7e5219b0ef2
YILANG_WU\yilan <yilangwu@outlook.com>
2023-04-20 05:23:01 +1000

    half lazyload

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
+import TempGraph from "./TempGraph";
 import styles from "./Contents.module.css";
+import WindGraph from "./WindGraph";
+import PressureGraph from "./PressureGraph";
 import { Suspense } from "react";
 import { DataContext } from "../store/DataContext";
 import { Empty, Spin } from "antd";
+import EmbeddedMap from "./Map";
 
-const Embedded = React.lazy(() => import("./Map.jsx"));
-const Temp = React.lazy(() => import("./TempGraph.jsx"));
-const Wind = React.lazy(() => import("./WindGraph.jsx"));
-const Pressure = React.lazy(() => import("./PressureGraph.jsx"));
 const Humidity = React.lazy(() => import("./HumidityGraph.jsx"));
 const Oktas = React.lazy(() => import("./OktasGraph.jsx"));
 const RainTrace = React.lazy(() => import("./RainTraceGraph.jsx"));
@@ -20,18 +20,10 @@ export const Contents = () => {
       {loading && <Spin size="large" />}
       {weatherData && (
         <div className={styles.container}>
-          <Suspense fallback={<Spin />}>
-            <Embedded />
-          </Suspense>
-          <Suspense fallback={<Spin />}>
-            <Temp />
-          </Suspense>
-          <Suspense fallback={<Spin />}>
-            <Wind />
-          </Suspense>
-          <Suspense fallback={<Spin />}>
-            <Pressure />
-          </Suspense>
+          <EmbeddedMap />
+          <TempGraph />
+          <WindGraph />
+          <PressureGraph />
           <Suspense fallback={<Spin />}>
             <Humidity />
           </Suspense>
