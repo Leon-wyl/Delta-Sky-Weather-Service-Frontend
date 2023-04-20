7941f9b51d7eb38b66642cb5e887592d0cb4787f
Girichand <g.jupudi@student.unsw.edu.au>
2023-04-20 22:39:18 +1000

    added state for the page

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Header from "./components/Header";
 import { Contents } from "./components/Contents";
 import { DataContext } from "./store/DataContext";
 
-function App() {
+function App () {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [station, setStation] = useState(null);
+  const [page, setPage] = useState('simple')
 
   return (
     <div className="App">
