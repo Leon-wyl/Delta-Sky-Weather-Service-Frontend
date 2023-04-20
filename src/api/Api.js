9aa207c3429692ca7ab06d4834a2853e676edf8e
YILANG_WU\yilan <yilangwu@outlook.com>
2023-04-20 14:50:08 +1000

    api for news

--- src/api/Api.js
+++ src/api/Api.js
@@ -18,41 +18,32 @@ export const fetchWindEfficiency = async (current_windspeed, max_windspeed) => {
   }
 };
 
-// export const fetchNews = async () => {
-//   try {
-//     const dataNews = JSON.stringify({
-//       query: `{
-//         contentSearch(q:{
-//           operation: AND,
-//           subQueries: [
-//             {term: "weather"},
-//             {term: "sydney"}
-//           ]
-//         }) {
-//           total
-//           results {
-//             webTitle
-//           }
-//         }
-//       }`,
-//     });
-//     const response = await fetch("/api/H09A_FOXTROT/graphql", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//         "Access-Control-Allow-Origin": "*",
-//       },
-//       body: dataNews,
-//     });
-//     console.log(response);
-
-//     const json = await response.json();
-//     console.log(json);
-//   } catch (err) {
-//     return err;
-//   }
-// };
+export const fetchNews = async (station) => {
+  try {
+    const url = "/api/H09A_FOXTROT/graphql"
+    const dataNews = {
+      query: `{
+        contentSearch(q:{
+          operation: AND,
+          subQueries: [
+            {term: "Sydney - Observatory Hill"},
+          ]
+        }) {
+          total
+          results {
+            webTitle
+            webUrl
+          }
+        }
+      }`,
+    };
+    const res = await postAxios(url, dataNews);
+    console.log(res);
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
 
 export const fetchWeatherData = async () => {
   try {
